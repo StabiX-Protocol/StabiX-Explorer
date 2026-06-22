25ff8d8ca7c38557b29c35abcaf2f03e59f9a5be
StabiX-Protocol <stabix10@gmail.com>
2026-06-22 09:19:19 +0530

    Update explorer.js

--- js/explorer.js
+++ js/explorer.js
@@ -23,7 +23,7 @@ const firebaseConfig = {
   projectId: "stabix-backend-v1",
   storageBucket: "stabix-backend-v1.firebasestorage.app",
   messagingSenderId: "351361221507",
-  appId: "1:351361221507:web:ebaf0d15e86d4b184c6cb6"
+  appId: "1:351361221507:web:ebaf0d15e86d4b184c6cb6",
   databaseURL: "https://stabix-backend-v1-default-rtdb.firebaseio.com"
 };
 
